7c4717cec6f3a23ce46fc435786ec92ea0f7c7f7
masahiko-1008 <138412620+masahiko-1008@users.noreply.github.com>
2024-01-17 15:09:10 +0900

    制作2

---
+++ Game/DriveAndAvoid/Object/Player.h
@@ -0,0 +1,33 @@
+#pragma oncce
+
+#include "../Utility/Vector2D.h"
+#include "Barrier.h"
+
+class Player
+{
+private:
+	bool is_active;     //有効状態か？
+	int image;          //画像データ
+	Vector2D location;  //位置座標
+	Vector2D box_size;  //当たり判定の大きさ
+	float angle;        //角度
+	float speed;        //速さ
+	float hp;           //体力
+	float fuel;         //燃料
+	int barrier_count;  //バリアの枚数
+	Barrier* barrier;   //バリア
+
+public:
+	Player();
+	~Player();
+
+	void Initialize();  //初期化処理
+	void Update();      //更新処理
+	void Draw();        //描画処理
+	void Finalize();    //終了時処理
+
+public:
+	void SetActive(bool flg);      //有効フラグ設定
+	void DereaseHp(float value);   //体力減少処理
+	Vector2D Get 
+};
